3ff295da180facf2d6fa13d4e2a0567be6af6a30
Thnorty <ototatik@gmail.com>
2025-03-12 13:14:33 +0300

    Add training script and update .gitignore for new outputs and logs

---
+++ start_training.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=start_llm_training_job   # Job name
+#SBATCH --output=logs/output.log            # Standard output log
+#SBATCH --error=logs/error.log              # Standard error log
+#SBATCH --time=02:00:00                     # Time limit hh:mm:ss
+#SBATCH --partition=compute                 # Partition name
+
+# Load necessary modules (if needed)
+module load miniconda
+eval "$(conda shell.bash hook)"
+conda activate env1
+module load cuda/12.4
+
+cd /home/kernelpanic/bitirme/LLM-Training/
+
+# Create logs directory if it doesn't exist
+mkdir -p logs
+
+# pip3.11 install -r requirements.txt
+
+# Run the training script
+echo "Training started at $(date)"
+jupyter nbconvert --to script unsloth-training.ipynb
+python unsloth-training.py
+echo "Training finished at $(date)"
